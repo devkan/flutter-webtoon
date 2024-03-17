093c2a10e1d771d6d069a9ac4e19db4721a1c655
Kan <secureis@gmail.com>
2024-03-17 14:16:52 +0900

    add comment

--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -6,4 +6,8 @@ class WebtoonModel {
       : title = json['title'],
         thumb = json['thumb'],
         id = json['id'];
+
+  // json으로 넘어오는 값은 
+  // {id: 819717, title: xxxxx, thumb: https://xxxx.jpg} 이런형태다.
+  // js의 json 형태랑 틀린것이 dart에서 이미 한번 가공이 된거 같다.
 }
